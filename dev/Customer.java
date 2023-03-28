465846cb88fe719b7e7c04eabd676cd4d7bfc0d4
Architect <architect@example.com>
2023-03-28 14:40:25 +0800

    Added customer model

---
+++ dev/Customer.java
@@ -0,0 +1,11 @@
+
+class Customer {
+
+    id customerID;
+    String customerName;
+
+    public Customer(id customerID, String customerName) {
+        this.customerID = customerID;
+        this.customerName = customerName;
+    }
+}
